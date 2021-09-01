03f1a88c68285d13f6f35cedf995ebf34a969632
Manali12 <manalichoudhari317@gmail.com>
2021-09-01 22:13:34 +0530

    Google auth with Firebase

--- src/Firebase/utilities.js
+++ src/Firebase/utilities.js
@@ -1,4 +1,23 @@
-import firebase from "firebase/app";
-import "firebase/auth"
-import "firebase/firestore"
 import { firebaseConfig } from "./config";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+// signInWithPopup(auth, provider)
+//     .then((result) => {
+//         const credential = GoogleAuthProvider.credentialFromResult(result);
+//         const token = credential.accessToken;
+//         const user = result.user;
+//     }).catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         const email = error.email;
+//         const credential = GoogleAuthProvider.credentialFromError(error);
+//     });
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import "../Login/Login.scss"
 import SignIn from '../../Components/SignIn/SignIn';
+import side1 from "../../images/Lback6.png"
+import side2 from "../../images/Lback4.png"
 
 
 export default function Login() {
     return (
-        <div>
+        <div className="login-layout">
+            <div className="login-wrap">
+                <div className="side-1"
+                    style={{
+                        backgroundImage: `url(${side1})`
+                    }}>
+                </div>
+                <div className="side-1"
+                    style={{
+                        backgroundImage: `url(${side2})`
+                    }}>
+                </div>
+            </div>
+            <div className="sign-in"><SignIn /></div>
 
         </div>
     )
